da26b34ef2b6b83adb6559d7645fff1d24a6fe75
IslasDulce <dulcesitoisa79@gmail.com>
2021-10-29 14:41:26 -0500

    backformulario

---
+++ src/main/java/com/consolecode/model/Persona.java
@@ -0,0 +1,94 @@
+package com.consolecode.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="persona")
+public class Persona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idPersona;
+	
+	@Column(name="nombres",length = 100, nullable = false)
+	private String nombres;
+	
+	@Column(name="apellidos",length = 100, nullable = false)
+	private String apellidos;
+	
+	@Column(name="edad")
+	private Integer edad;
+	
+	@Column(name="sexo",length = 100, nullable = false)
+	private String sexo;
+	
+	@Column(name="fecha_nacimiento")
+	private String fecha_nacimiento; 
+	
+	@ManyToOne
+	@JoinColumn(name="idPais")
+	private Pais pais;
+	
+	public Integer getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(Integer idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	} 
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+}
